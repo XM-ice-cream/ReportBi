df414a5dd528bc1920975734b8266d7a118febf5
xinming_chen <XinmingXM.Chen@Luxhshare-ict.com>
2021-11-24 17:03:06 +0800

    去掉pitch 查询Warferd必填卡控

--- src/views/bill-manage/expandpitch-query.vue
+++ src/views/bill-manage/expandpitch-query.vue
@@ -7,58 +7,23 @@
         <div slot="title">
           <Row>
             <i-col span="6">
-              <Poptip
-                v-model="searchPoptipModal"
-                class="poptip-style"
-                placement="right-start"
-                width="500"
-                trigger="manual"
-                transfer
-              >
-                <Button
-                  type="primary"
-                  icon="ios-search"
-                  @click.stop="searchPoptipModal = !searchPoptipModal"
-                >
+              <Poptip v-model="searchPoptipModal" class="poptip-style" placement="right-start" width="500" trigger="manual" transfer>
+                <Button type="primary" icon="ios-search" @click.stop="searchPoptipModal = !searchPoptipModal">
                   {{ $t("selectQuery") }}
                 </Button>
                 <div class="poptip-style-content" slot="content">
-                  <Form
-                    ref="searchReq"
-                    :model="req"
-                    :label-width="80"
-                    :label-colon="true"
-                    @submit.native.prevent
-                    @keyup.native.enter="searchClick"
-                  >
+                  <Form ref="searchReq" :model="req" :label-width="80" :label-colon="true" @submit.native.prevent @keyup.native.enter="searchClick">
                     <!-- 起始时间 -->
                     <FormItem :label="$t('startTime')" prop="startTime">
-                      <DatePicker
-                        transfer
-                        type="datetime"
-                        :placeholder="$t('pleaseSelect') + $t('startTime')"
-                        format="yyyy-MM-dd HH:mm:ss"
-                        :options="$config.datetimeOptions"
-                        v-model="req.startTime"
-                      ></DatePicker>
+                      <DatePicker transfer type="datetime" :placeholder="$t('pleaseSelect') + $t('startTime')" format="yyyy-MM-dd HH:mm:ss" :options="$config.datetimeOptions" v-model="req.startTime"></DatePicker>
                     </FormItem>
                     <!-- 结束时间 -->
                     <FormItem :label="$t('endTime')" prop="endTime">
-                      <DatePicker
-                        transfer
-                        type="datetime"
-                        :placeholder="$t('pleaseSelect') + $t('endTime')"
-                        format="yyyy-MM-dd HH:mm:ss"
-                        :options="$config.datetimeOptions"
-                        v-model="req.endTime"
-                      ></DatePicker>
+                      <DatePicker transfer type="datetime" :placeholder="$t('pleaseSelect') + $t('endTime')" format="yyyy-MM-dd HH:mm:ss" :options="$config.datetimeOptions" v-model="req.endTime"></DatePicker>
                     </FormItem>
                     <!-- 大板序号 -->
                     <FormItem label="WarferID">
-                      <Input
-                        v-model.trim="req.barcode"
-                        :placeholder="$t('pleaseEnter') + 'WarferID' + $t('multiple,separated')"
-                      ></Input>
+                      <Input v-model.trim="req.barcode" :placeholder="$t('pleaseEnter') + 'WarferID' + $t('multiple,separated')"></Input>
                     </FormItem>
                   </Form>
                   <div class="poptip-style-button">
@@ -73,23 +38,9 @@
             </i-col>
           </Row>
         </div>
-        <Table
-          :border="tableConfig.border"
-          :highlight-row="tableConfig.highlightRow"
-          :height="tableConfig.height"
-          :loading="tableConfig.loading"
-          :columns="columns"
-          :data="data"
-        >
+        <Table :border="tableConfig.border" :highlight-row="tableConfig.highlightRow" :height="tableConfig.height" :loading="tableConfig.loading" :columns="columns" :data="data">
         </Table>
-        <page-custom
-          :total="req.total"
-          :totalPage="req.totalPage"
-          :pageIndex="req.pageIndex"
-          :page-size="req.pageSize"
-          @on-change="pageChange"
-          @on-page-size-change="pageSizeChange"
-        />
+        <page-custom :total="req.total" :totalPage="req.totalPage" :pageIndex="req.pageIndex" :page-size="req.pageSize" @on-change="pageChange" @on-page-size-change="pageSizeChange" />
       </Card>
     </div>
   </div>
@@ -100,7 +51,7 @@ import { getpagelistReq, exportReq } from "@/api/bill-manage/expandpitch-query";
 import { getButtonBoolean, formatDate, exportFile, commaSplitString } from "@/libs/tools";
 export default {
   name: "expandpitch-query",
-  data() {
+  data () {
     return {
       searchPoptipModal: false,
       noRepeatRefresh: true, //刷新数据的时候不重复刷新pageLoad
@@ -136,64 +87,60 @@ export default {
       ], // 表格数据
     };
   },
-  activated() {
+  activated () {
     this.pageLoad();
     this.autoSize();
     window.addEventListener("resize", () => this.autoSize());
     getButtonBoolean(this, this.btnData);
   },
   // 导航离开该组件的对应路由时调用
-  beforeRouteLeave(to, from, next) {
+  beforeRouteLeave (to, from, next) {
     this.searchPoptipModal = false;
     next();
   },
   methods: {
     // 点击搜索按钮触发
-    searchClick() {
+    searchClick () {
       this.req.pageIndex = 1;
       this.pageLoad();
     },
     // 获取分页列表数据
-    pageLoad() {
+    pageLoad () {
       this.data = [];
       this.tableConfig.loading = false;
       let { startTime, endTime, barcode } = this.req;
-      if ( barcode) {
-        this.$refs.searchReq.validate((validate) => {
-          if (validate) {
-            this.tableConfig.loading = true;
-            let obj = {
-              orderField: 'barcode', // 排序字段
-              ascending: true, // 是否升序
-              pageSize: this.req.pageSize, // 分页大小
-              pageIndex: this.req.pageIndex, // 当前页码
-              data: {
-                startTime: formatDate(startTime),
-                endTime: formatDate(endTime),
-                barcode: commaSplitString(barcode).join(),
-              },
-            };
-            getpagelistReq(obj)
-              .then((res) => {
-                this.tableConfig.loading = false;
-                if (res.code === 200) {
-                  let { data, pageSize, pageIndex, total, totalPage } = res.result;
-                  this.data = data || [];
-                  this.req = { ...this.req, pageSize, pageIndex, total, totalPage };
-                  this.searchPoptipModal = false;
-                }
-              })
-              .catch(() => (this.tableConfig.loading = false));
-          }
-        });
-      } else {
-        this.$Message.warning(this.$t("pleaseEnter") + 'WarferID');
-      }
+      this.$refs.searchReq.validate((validate) => {
+        if (validate) {
+          this.tableConfig.loading = true;
+          let obj = {
+            orderField: 'barcode', // 排序字段
+            ascending: true, // 是否升序
+            pageSize: this.req.pageSize, // 分页大小
+            pageIndex: this.req.pageIndex, // 当前页码
+            data: {
+              startTime: formatDate(startTime),
+              endTime: formatDate(endTime),
+              barcode: commaSplitString(barcode).join(),
+            },
+          };
+          getpagelistReq(obj)
+            .then((res) => {
+              this.tableConfig.loading = false;
+              if (res.code === 200) {
+                let { data, pageSize, pageIndex, total, totalPage } = res.result;
+                this.data = data || [];
+                this.req = { ...this.req, pageSize, pageIndex, total, totalPage };
+                this.searchPoptipModal = false;
+              }
+            })
+            .catch(() => (this.tableConfig.loading = false));
+        }
+      });
     },
     // 导出
-    exportClick() {
+    exportClick () {
       let { startTime, endTime, barcode } = this.req;
-      if ( barcode) {
+      if (barcode) {
         let obj = {
           startTime: formatDate(startTime),
           endTime: formatDate(endTime),
@@ -205,24 +152,24 @@ export default {
           exportFile(blob, fileName);
         });
       } else {
-       this.$Message.warning(this.$t("pleaseEnter") + 'WarferID');
+        this.$Message.warning(this.$t("pleaseEnter") + 'WarferID');
       }
     },
     // 点击重置按钮触发
-    resetClick() {
+    resetClick () {
       this.$refs.searchReq.resetFields();
     },
     // 自动改变表格高度
-    autoSize() {
+    autoSize () {
       this.tableConfig.height = document.body.clientHeight - 120 - 60;
     },
     // 选择第几页
-    pageChange(index) {
+    pageChange (index) {
       this.req.pageIndex = index;
       this.pageLoad();
     },
     // 选择一页有条数据
-    pageSizeChange(index) {
+    pageSizeChange (index) {
       this.req.pageIndex = 1;
       this.req.pageSize = index;
       this.pageLoad();
