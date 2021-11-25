75e399299a779200f156bdb723ed686741f16c51
xinming_chen <XinmingXM.Chen@Luxhshare-ict.com>
2021-11-25 08:31:13 +0800

    删除receive data 页面地址写死问题

--- src/api/bill-manage/receive-data.js
+++ src/api/bill-manage/receive-data.js
@@ -7,7 +7,7 @@ import {
 export const getpagelistReq = data => {
     return requestCenter({
         timeout: 0,
-        baseUrl: 'http://172.30.50.14:8080' || window.localStorage.getItem("reportip"),
+        baseUrl: window.localStorage.getItem("reportip"),
         url: '/api/reportcenter/anonymous/v1/receivedataquery/getreceivedata',
         method: 'post',
         data
@@ -17,7 +17,7 @@ export const getpagelistReq = data => {
 export const exportReq = data => {
     return requestCenter({
         timeout: 0,
-        baseUrl: 'http://172.30.50.14:8080' || window.localStorage.getItem("reportip"),
+        baseUrl: window.localStorage.getItem("reportip"),
         url: '/api/reportcenter/anonymous/v1/receivedataquery/export',
         method: 'post',
         responseType: 'arraybuffer',
