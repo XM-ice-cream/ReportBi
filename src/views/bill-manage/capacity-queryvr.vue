cbbf968ab77ea31cc02a8ddcfb9584a3ed870722
葛文青 <Winson.Ge@luxshare-ict.com>
2022-01-08 15:55:44 +0800

    产能报表添加“机种名称”显示字段，并取消打开页面自动加载； EncapOP70报表新增“Dwell&PreCure Time”显示字段

--- src/views/bill-manage/capacity-queryvr.vue
+++ src/views/bill-manage/capacity-queryvr.vue
@@ -212,6 +212,14 @@ export default {
           align: "center",
         },
         {
+          title: this.$t("modelName"),
+          key: "modelname",
+          minWidth: 100,
+          ellipsis: true,
+          tooltip: true,
+          align: "center",
+        },
+        {
           title: this.$t("stationId"),
           key: "processName",
           minWidth: 140,
@@ -586,6 +594,7 @@ export default {
     this.autoSize();
     window.addEventListener("resize", () => this.autoSize());
     getButtonBoolean(this, this.btnData);
+    this.tableConfig.loading = false;//打开页面后不会有加载动画
   },
   deactivated() {
     this.searchPoptipModal = false;
@@ -725,7 +734,7 @@ export default {
     },
   },
   mounted() {
-    this.pageLoad();
+    //this.pageLoad();
   },
 };
 </script>
