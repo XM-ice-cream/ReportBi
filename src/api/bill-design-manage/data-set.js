d74798bb703417fc2850608d527010a0f9312018
土豆泥拌辣椒 <1164392802@qq.com>
2022-06-13 13:17:52 +0800

    修改测试预览限时20 S

--- src/api/bill-design-manage/data-set.js
+++ src/api/bill-design-manage/data-set.js
@@ -50,6 +50,7 @@ export const modifyDatacollectReq = data => {
 // 测试
 export const testTransformSet = data => {
     return requestCenter({
+        timeout: 1000 * 60,
         baseUrl: "http://10.191.19.111:8700" || window.localStorage.getItem("reportDesignIp"),
         url: '/api/autoreportcenter/anonymous/v1/datacollect/testtransform',
         method: 'post',
