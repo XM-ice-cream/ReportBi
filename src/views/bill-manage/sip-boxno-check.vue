ab06371289e86691396acea5384f3d9708b84c58
xinming_chen <XinmingXM.Chen@Luxhshare-ict.com>
2022-07-22 10:24:07 +0800

    SIP 包装箱号核验优化

--- src/views/bill-manage/sip-boxno-check.vue
+++ src/views/bill-manage/sip-boxno-check.vue
@@ -3,61 +3,66 @@
   <div class="page-style">
     <!-- 页面输入下载条件 -->
     <div class="comment">
-      <Card :bordered="false" dis-hover class="card-style" id="sip-boxno-check">      
-          <Form ref="submitReq" :model="req" :label-width="150" :label-colon="false" style="width: 80%;margin:0 atuo" @submit.native.prevent>
-            <Row>
-                <Col span="8">
-                    <FormItem label='包装箱号' prop="cartonNo">
-                        <label>{{req.cartonNo}}</label>
-                    </FormItem>
-                </Col>
-                <Col span="8">
-                    <FormItem label='料号' prop="pn">
-                        <label>{{req.pn}}</label>
-                    </FormItem>
-                </Col>
-                <Col span="8">
-                    <FormItem label='vendorNO' prop="vendorNO">
-                        <label>{{req.vendorNO}}</label>
-                    </FormItem>
-                </Col>
-                <Col span="8">
-                    <FormItem label='QTY' prop="cartonQTY">
-                         <label>{{req.cartonQTY}}</label>
-                    </FormItem>
-                </Col>
-                <Col span="8">
-                    <FormItem label='配送地' prop="shipAddr">
-                         <label> {{req.shipAddr}}</label>
-                    </FormItem>
-                </Col>
-                <Col span="24">
-                    <FormItem label='UnitID' prop="unitID">
-                        <Input type="text" ref="inputUnitId" v-model="req.unitID" clearable size="default" placeholder="请刷入UnitID" @on-enter="getBoxNoInfo"></Input>
-                    </FormItem>
-                </Col>
-                <Col span="24">
-                    <FormItem label='内箱码' prop="inputBoxNo">
-                         <Input type="text" v-model="req.inputBoxNo" :disabled = "!isInputNo" ref="inputBoxNo" clearable size="default" placeholder="请刷入内箱码" @on-enter="checkBoxNo"></Input>
-                    </FormItem>
-                </Col>
-                 <Col span="24">
-                    <FormItem label='外箱码' prop="inputCartonNo">
-                         <Input type="text" v-model="req.inputCartonNo" :disabled = "!isInputNo" ref="inputCartonNo" clearable size="default" placeholder="请刷入外箱码" @on-enter="checkCartonNo"></Input>
-                    </FormItem>
-                </Col>                
-                <Col span="24">
-                    <FormItem>
-                         <Button type="primary" @click="reset()" size="default">{{ $t("reset") }}</Button>
-                    </FormItem>
-                </Col>            
-             </Row>
-          </Form>
+      <Card :bordered="false" dis-hover class="card-style" id="sip-boxno-check">
+        <Form ref="submitReq" :model="req" :label-width="80" :label-colon="false" style="width: 80%;margin:0 atuo" @submit.native.prevent>
+          <Row>
+            <Col :xs="24" :sm="24" :md="12" :lg="8">
+            <FormItem label='外箱号' prop="cartonNo">
+              <label>{{req.cartonNo}}</label>
+            </FormItem>
+            </Col>
+            <Col :xs="24" :sm="24" :md="12" :lg="8">
+            <FormItem label='内箱号' prop="boxNo">
+              <label>{{req.boxNo}}</label>
+            </FormItem>
+            </Col>
+            <Col :xs="24" :sm="24" :md="12" :lg="8">
+            <FormItem label='料号' prop="pn">
+              <label>{{req.pn}}</label>
+            </FormItem>
+            </Col>
+            <Col :xs="24" :sm="24" :md="12" :lg="8">
+            <FormItem label='客户料号' prop="apn">
+              <label>{{req.apn}}</label>
+            </FormItem>
+            </Col>
+            <Col :xs="24" :sm="24" :md="12" :lg="8">
+            <FormItem label='QTY' prop="cartonQTY">
+              <label>{{req.cartonQTY}}</label>
+            </FormItem>
+            </Col>
+            <Col :xs="24" :sm="24" :md="12" :lg="8">
+            <FormItem label='配送地' prop="shipAddr">
+              <label> {{req.shipAddr}}</label>
+            </FormItem>
+            </Col>
+            <Col span="24">
+            <FormItem label='UnitID' prop="unitID">
+              <Input type="text" ref="inputUnitId" v-model="req.unitID" clearable size="default" placeholder="请刷入UnitID" @on-enter="getBoxNoInfo"></Input>
+            </FormItem>
+            </Col>
+            <Col span="24">
+            <FormItem label='内箱码' prop="inputBoxNo">
+              <Input type="text" v-model="req.inputBoxNo" :disabled="!isInputNo" ref="inputBoxNo" clearable size="default" placeholder="请刷入内箱码" @on-enter="checkBoxNo"></Input>
+            </FormItem>
+            </Col>
+            <Col span="24">
+            <FormItem label='外箱码' prop="inputCartonNo">
+              <Input type="text" v-model="req.inputCartonNo" :disabled="!isInputNo" ref="inputCartonNo" clearable size="default" placeholder="请刷入外箱码" @on-enter="checkCartonNo"></Input>
+            </FormItem>
+            </Col>
+            <Col span="24">
+            <FormItem>
+              <Button type="primary" @click="reset()" size="default">{{ $t("reset") }}</Button>
+            </FormItem>
+            </Col>
+          </Row>
+        </Form>
         <!-- 提交记录 -->
         <div v-if="tipMsg">
-            <Alert :type="tipMsg.indexOf('NG')==-1?'success':'error'" show-icon style="font-size: 1.2rem;">
-             {{tipMsg}}
-            </Alert>
+          <Alert :type="tipMsg.indexOf('NG')==-1?'success':'error'" show-icon style="font-size: 1.2rem;">
+            {{tipMsg}}
+          </Alert>
         </div>
       </Card>
     </div>
@@ -75,68 +80,72 @@ export default {
       req: {
         unitID: "",
         pn: "",
-        vendorNO: null,//厂商NO
+        apn: null,//客户料号
         cartonNo: "",
-        inputCartonNo:"",//输入的外箱条码
+        inputCartonNo: "",//输入的外箱条码
         boxNo: "",
-        inputBoxNo:"", //输入的内箱条码
-        shipAddr:"", //配送地
-        cartonQTY: "", 
+        inputBoxNo: "", //输入的内箱条码
+        shipAddr: "", //配送地
+        cartonQTY: "",
       },
-      tipMsg:"",//提示信息
-      isInputNo:false,//是否可输入箱号核对
+      tipMsg: "",//提示信息
+      isInputNo: false,//是否可输入箱号核对
     };
   },
   methods: {
     //获取信息
     getBoxNoInfo () {
-          const { unitID} = this.req;
-          const obj = { unitID };
-          checkBoxNoReq(obj).then(res => {
-            if (res.code === 200) {     
-              if(!res.result){
-                this.tipMsg = "NG 未查到此条码，请确认";
-                this.isInputNo = false;//不可输入箱号核对
-                this.req= {...this.req, pn: "",vendorNO: null,cartonNo: "",inputCartonNo:"",boxNo: "", inputBoxNo:"", shipAddr:"",cartonQTY: "",};
-                return;
-              };    
-                     
-             this.req = {...this.req,...res.result,inputBoxNo:""};
-             this.tipMsg = "";
-             this.isInputNo = true;//可输入箱号核对
-             //输入框聚焦
-             inputSelectContent(this.$refs.inputBoxNo);
-             
-            } 
-          })
+      const { unitID } = this.req;
+      const obj = { unitID };
+      checkBoxNoReq(obj).then(res => {
+        if (res.code === 200) {
+          if (!res.result) {
+            this.tipMsg = "NG 未查到此条码，请确认";
+            this.isInputNo = false;//不可输入箱号核对
+            this.req = { ...this.req, pn: "", apn: null, cartonNo: "", inputCartonNo: "", boxNo: "", inputBoxNo: "", shipAddr: "", cartonQTY: "", };
+            return;
+          };
+
+          this.req = { ...this.req, ...res.result, inputBoxNo: "" };
+          this.tipMsg = "";
+          this.isInputNo = true;//可输入箱号核对
+          //输入框聚焦
+          inputSelectContent(this.$refs.inputBoxNo);
+
+        }
+      })
     },
     //核验内箱号
-    checkBoxNo(){
-        const { boxNo,inputBoxNo} = this.req;
-        if(boxNo===inputBoxNo){
-             //输入框聚焦
-             inputSelectContent(this.$refs.inputCartonNo);
-        }else{
-            this.tipMsg = "NG 检查失败 与内箱条码不匹配!";
-        }
+    checkBoxNo () {
+      const { boxNo, inputBoxNo, pn } = this.req;
+      const checkInput = inputBoxNo.split("$");
+
+      if (boxNo === checkInput[0] && pn === checkInput[3]) {
+        //输入框聚焦
+        inputSelectContent(this.$refs.inputCartonNo);
+        this.tipMsg = "";
+      } else {
+        this.tipMsg = "NG 检查失败 与内箱条码或者料号不匹配!";
+      }
     },
     // 核验外箱码
-    checkCartonNo(){
-        const { cartonNo,inputCartonNo} = this.req;
-        if(cartonNo===inputCartonNo){
-            this.tipMsg = "OK 检查成功";
-        }else{
-            this.tipMsg = "NG 检查失败 与外箱条码不匹配!";
-        }
+    checkCartonNo () {
+      const { cartonNo, inputCartonNo, pn } = this.req;
+      const checkInput = inputCartonNo.split("$");
+      if (cartonNo === checkInput[0] && pn === checkInput[3]) {
+        this.tipMsg = "OK 检查成功";
+      } else {
+        this.tipMsg = "NG 检查失败 与外箱条码或者料号不匹配!";
+      }
     },
     // 重置
     reset () {
       this.$refs.submitReq.resetFields();
-       inputSelectContent(this.$refs.inputUnitId);
+      inputSelectContent(this.$refs.inputUnitId);
     }
   },
   mounted () {
-       inputSelectContent(this.$refs.inputUnitId);
+    inputSelectContent(this.$refs.inputUnitId);
   },
 }
 </script>
@@ -162,7 +171,7 @@ export default {
   /deep/.ivu-FormItem {
     margin-bottom: 25px;
   }
-  label{
+  label {
     padding-left: 1rem;
     background: #f5f7f9;
     border-radius: 3px;
