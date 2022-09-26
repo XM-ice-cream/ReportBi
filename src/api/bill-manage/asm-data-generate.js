7389629abf63ae4a1f38b352fe737d837c886509
xinming_chen <XinmingXM.Chen@Luxhshare-ict.com>
2022-09-26 15:58:19 +0800

    去除 ASM数据处理 固定ip

--- src/api/bill-manage/asm-data-generate.js
+++ src/api/bill-manage/asm-data-generate.js
@@ -7,7 +7,7 @@ import {
 export const addReq = data => {
     return requestCenter({
         timeout: 0,
-        baseUrl: "http://10.32.44.112:5700" || window.localStorage.getItem("reportip"),
+        baseUrl: window.localStorage.getItem("reportip"),
         url: '/api/reportcenter/anonymous/v1/asmgeneratedata/add',
         method: 'get',
         params: data
