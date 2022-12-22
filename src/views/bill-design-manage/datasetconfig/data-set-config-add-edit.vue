ac712295eaf50ce556ed6df27df201b971b516c9
xinming_chen <XinmingXM.Chen@Luxhshare-ict.com>
2022-12-22 15:35:39 +0800

    增加卡片抖动效果

--- src/views/bill-design-manage/datasetconfig/data-set-config-add-edit.vue
+++ src/views/bill-design-manage/datasetconfig/data-set-config-add-edit.vue
@@ -9,7 +9,7 @@
 				</div>
 			</template>
 			<div class="modal-content">
-				<div class="base-info">
+				<div class="base-info" :class="[isShake ? 'shake-constant' : '']">
 					<Form ref="submitRef" :model="submitData" inline :label-width="90" :rules="rulesValidate">
 						<FormItem label="数据集名称:" prop="datasetName">
 							<Input v-model="submitData.datasetName" :placeholder="$t('pleaseEnter') + '数据集名称'" />
@@ -109,6 +109,7 @@ export default {
 		return {
 			modalTitle: "数据集配置",
 			connectModalFlag: false,
+			isShake: false, //右侧卡片是否抖动
 			connectObj: {}, //关联表
 			submitData: { datasetName: "", datasetCode: "", sourceCode: "", filterTable: "", enabled: 1, content: "" },
 			splitValue: 0.2,
@@ -189,6 +190,7 @@ export default {
 			//return;
 			this.$refs.submitRef.validate((validate) => {
 				if (validate) {
+					this.isShake = false; //停止抖动
 					const requestApi = this.isAdd ? addReq(obj) : modifyReq(obj);
 					requestApi.then((res) => {
 						if (res.code === 200) {
@@ -199,6 +201,11 @@ export default {
 							this.$Message.error("提交异常", res.message);
 						}
 					});
+				} else {
+					this.isShake = true;
+					setTimeout(() => {
+						this.isShake = false;
+					}, 1000 * 0.8); //0.8秒后抖动恢复原值
 				}
 			});
 		},
@@ -455,6 +462,7 @@ export default {
 				combos: [],
 			};
 			this.createGraphic();
+			this.isShake = flase;
 		},
 	},
 };
@@ -462,7 +470,7 @@ export default {
 <style lang="less" scoped>
 .base-info {
 	position: absolute;
-	right: 0px;
+	right: 10px;
 	background: #72c424;
 	margin: 5px;
 	/* line-height: 40px; */
@@ -472,7 +480,6 @@ export default {
 	border-radius: 5px;
 	color: #fff !important;
 	width: 14%;
-	float: right;
 	z-index: 9999999;
 	:deep(.ivu-form .ivu-form-item-label) {
 		color: #fff;
@@ -557,4 +564,163 @@ export default {
 // :deep(.ivu-modal-fullscreen .ivu-modal-body) {
 // 	top: 20px;
 // }
+.shake-constant {
+	animation-name: shake-hard;
+	animation-duration: 0.8s; //动画持续时间
+	animation-timing-function: ease-in-out;
+	// animation-iteration-count: infinite; /*定义循环资料，infinite为无限次*/
+}
+@keyframes shake-hard {
+	2% {
+		transform: translate(3px, -8px) rotate(-0.5deg);
+	}
+	4% {
+		transform: translate(-9px, 5px) rotate(-0.5deg);
+	}
+	6% {
+		transform: translate(-6px, 9px) rotate(2.5deg);
+	}
+	8% {
+		transform: translate(-8px, 9px) rotate(0.5deg);
+	}
+	10% {
+		transform: translate(-6px, -1px) rotate(0.5deg);
+	}
+	12% {
+		transform: translate(-9px, 2px) rotate(2.5deg);
+	}
+	14% {
+		transform: translate(-2px, 9px) rotate(-1.5deg);
+	}
+	16% {
+		transform: translate(0px, 10px) rotate(-0.5deg);
+	}
+	18% {
+		transform: translate(-3px, -3px) rotate(3.5deg);
+	}
+	20% {
+		transform: translate(8px, -1px) rotate(3.5deg);
+	}
+	22% {
+		transform: translate(10px, -4px) rotate(-0.5deg);
+	}
+	24% {
+		transform: translate(0px, -8px) rotate(0.5deg);
+	}
+	26% {
+		transform: translate(-1px, 2px) rotate(-1.5deg);
+	}
+	28% {
+		transform: translate(8px, 8px) rotate(-1.5deg);
+	}
+	30% {
+		transform: translate(-9px, 5px) rotate(-0.5deg);
+	}
+	32% {
+		transform: translate(1px, 10px) rotate(1.5deg);
+	}
+	34% {
+		transform: translate(7px, -4px) rotate(3.5deg);
+	}
+	36% {
+		transform: translate(2px, -8px) rotate(-1.5deg);
+	}
+	38% {
+		transform: translate(6px, 10px) rotate(-2.5deg);
+	}
+	40% {
+		transform: translate(3px, -1px) rotate(0.5deg);
+	}
+	42% {
+		transform: translate(-5px, -4px) rotate(-0.5deg);
+	}
+	44% {
+		transform: translate(-3px, 10px) rotate(-2.5deg);
+	}
+	46% {
+		transform: translate(-7px, 2px) rotate(-2.5deg);
+	}
+	48% {
+		transform: translate(-5px, -1px) rotate(3.5deg);
+	}
+	50% {
+		transform: translate(-7px, -1px) rotate(1.5deg);
+	}
+	52% {
+		transform: translate(2px, 8px) rotate(-1.5deg);
+	}
+	54% {
+		transform: translate(7px, -9px) rotate(0.5deg);
+	}
+	56% {
+		transform: translate(-4px, 1px) rotate(1.5deg);
+	}
+	58% {
+		transform: translate(-2px, -8px) rotate(1.5deg);
+	}
+	60% {
+		transform: translate(-7px, 1px) rotate(-0.5deg);
+	}
+	62% {
+		transform: translate(-5px, -2px) rotate(-0.5deg);
+	}
+	64% {
+		transform: translate(-2px, 5px) rotate(-2.5deg);
+	}
+	66% {
+		transform: translate(-2px, 7px) rotate(3.5deg);
+	}
+	68% {
+		transform: translate(-7px, -1px) rotate(-0.5deg);
+	}
+	70% {
+		transform: translate(-5px, 8px) rotate(-2.5deg);
+	}
+	72% {
+		transform: translate(-3px, -9px) rotate(-2.5deg);
+	}
+	74% {
+		transform: translate(-2px, -7px) rotate(3.5deg);
+	}
+	76% {
+		transform: translate(-5px, -4px) rotate(2.5deg);
+	}
+	78% {
+		transform: translate(-2px, 10px) rotate(-1.5deg);
+	}
+	80% {
+		transform: translate(4px, 9px) rotate(3.5deg);
+	}
+	82% {
+		transform: translate(3px, -1px) rotate(-1.5deg);
+	}
+	84% {
+		transform: translate(4px, -6px) rotate(0.5deg);
+	}
+	86% {
+		transform: translate(-1px, 4px) rotate(-0.5deg);
+	}
+	88% {
+		transform: translate(10px, -5px) rotate(3.5deg);
+	}
+	90% {
+		transform: translate(-3px, 7px) rotate(-0.5deg);
+	}
+	92% {
+		transform: translate(5px, -2px) rotate(2.5deg);
+	}
+	94% {
+		transform: translate(-2px, -7px) rotate(-0.5deg);
+	}
+	96% {
+		transform: translate(0px, 10px) rotate(-2.5deg);
+	}
+	98% {
+		transform: translate(-4px, 3px) rotate(2.5deg);
+	}
+	0%,
+	100% {
+		transform: translate(0, 0) rotate(0);
+	}
+}
 </style>
